07505f2cf4093d48db16b8dc9db0b494e64343d2
yusuke sugiyama <yus-sugiyama@uninosushi.com>
2022-07-29 21:01:16 +0900

    items.serviceのテストコード作成

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,4 +1,4 @@
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { UserStatus } from '../auth/user-status.enum';
 import { ItemStatus } from './item-status.enum';
@@ -9,6 +9,9 @@ import { ItemsService } from './items.service';
 const mockItemRepository = () => ({
   find: jest.fn(),
   findOne: jest.fn(),
+  createItem: jest.fn(),
+  save: jest.fn(),
+  delete: jest.fn(),
 });
 
 const mockUser1 = {
@@ -57,6 +60,7 @@ describe('ItemsServiceTest', () => {
     });
   });
 
+  // findById メソッドのテスト実施
   describe('findById', () => {
     it('正常系', async () => {
       const expected = {
@@ -81,6 +85,86 @@ describe('ItemsServiceTest', () => {
       itemRepository.findOne.mockResolvedValue(null);
       await expect(itemsService.findById('test-id')).rejects.toThrow(
         NotFoundException,
+      );
+    });
+  });
+
+  // create メソッドのテスト実施
+  describe('create', () => {
+    it('正常系', async () => {
+      const expected = {
+        id: 'test-id',
+        name: 'PC',
+        price: 50000,
+        description: '',
+        status: ItemStatus.ON_SALE,
+        createdAt: '',
+        updatedAt: '',
+        userId: mockUser1.id,
+        user: mockUser1,
+      };
+
+      itemRepository.createItem.mockResolvedValue(expected);
+      const result = await itemsService.create(
+        { name: 'PC', price: 50000, description: '' },
+        mockUser1,
+      );
+      expect(result).toEqual(expected);
+    });
+  });
+
+  // updateStatus メソッドのテスト実施
+  describe('updateStatus', () => {
+    const mockItem = {
+      id: 'test-id',
+      name: 'PC',
+      price: 50000,
+      description: '',
+      status: ItemStatus.ON_SALE,
+      createdAt: '',
+      updatedAt: '',
+      userId: mockUser1.id,
+      user: mockUser1,
+    };
+    it('正常系', async () => {
+      itemRepository.findOne.mockResolvedValue(mockItem);
+      await itemsService.updateStatus('test-id', mockUser2);
+      // itemRepository のsave メソッド呼び出しをテスト(更新のため期待値比較は行わない)
+      expect(itemRepository.save).toHaveBeenCalled();
+    });
+
+    it('異常系：自身の商品を購入', async () => {
+      itemRepository.findOne.mockResolvedValue(mockItem);
+      await expect(
+        itemsService.updateStatus('test-id', mockUser1),
+      ).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  // delete メソッドのテスト実施
+  describe('delete', () => {
+    const mockItem = {
+      id: 'test-id',
+      name: 'PC',
+      price: 50000,
+      description: '',
+      status: ItemStatus.ON_SALE,
+      createdAt: '',
+      updatedAt: '',
+      userId: mockUser1.id,
+      user: mockUser1,
+    };
+    it('正常系', async () => {
+      itemRepository.findOne.mockResolvedValue(mockItem);
+      await itemsService.delete('test-id', mockUser1);
+      // itemRepository のdelete メソッド呼び出しをテスト(更新のため期待値比較は行わない)
+      expect(itemRepository.delete).toHaveBeenCalled();
+    });
+
+    it('異常系：他人の商品を削除', async () => {
+      itemRepository.findOne.mockResolvedValue(mockItem);
+      await expect(itemsService.delete('test-id', mockUser2)).rejects.toThrow(
+        BadRequestException,
       );
     });
   });
